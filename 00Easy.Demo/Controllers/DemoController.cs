529717cef361ff3ecc831e1329aa78cebab3abed
yangli_sky@163.com <yangli_sky@163.com>
2016-05-25 17:29:03 +0800

    a

---
+++ 00Easy.Demo/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Easy.Application;
+using Easy.Domain.Application;
+
+namespace _00Easy.Demo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            IReturn @IReturn = ApplicationRegistry.Demo.SayHello("zhangsan", 20);
+            var result = @IReturn.Result(new ReturnContext() { SystemId = "app" });
+
+            ViewBag.data = result;
+            return View();
+        }
+
+
+    }
+}
